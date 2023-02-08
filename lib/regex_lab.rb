eed95d60a0ba43712a091bd9dd720e3eff73276b
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-08 18:13:37 +0300

    2 test failing

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,38 @@
+require "pry"
+
+# \A indicates the first character 
 def starts_with_a_vowel?(word)
-
+    if word[0].match?(/[aeiou]/)
+        true
+    else
+        false
+    end
 end
 
+# \b is a word boundary, which matches the position between a word character
+# \w+ matches one or more word characters.
 def words_starting_with_un_and_ending_with_ing(text)
-
+      text.scan(/\bun\w+ing\b/)   
 end
 
+# \b is a word boundary, which matches the position between a word character
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
+# used unless bcoz its cleaner and straight forword 
+#  the first elem [0] used match to check if it starts with [A-Z]
+# the last [-1] checked panctuation ([\.\?\!]   )
 def first_word_capitalized_and_ends_with_punctuation?(text)
+    return false unless text[0].match?(/[A-Z]/)
+    return false unless text[-1].match?(/[\.\?\!]/)
+    true
+  end
+  
+  
+  
 
-end
-
+  binding.pry
 def valid_phone_number?(phone)
-
+    
 end
